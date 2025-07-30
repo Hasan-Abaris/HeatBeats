a747402a70bc346883717a3ace7aee9cf5ffabf6
iamsayyadhasan <hasanshahzad82@gmail.com>
2025-07-30 14:53:32 +0530

    Pushing my updated code to shared GitLab repo

---
+++ components/common/Header/data/mockCategories.js
@@ -0,0 +1,32 @@
+// /data/mockCategories.js
+
+const categories = [
+  {
+    name: 'Artificial Intelligence',
+    subcategories: [
+      {
+        name: 'Machine Learning',
+        courses: ['Intro to ML', 'ML with Python', 'ML for Beginners'],
+      },
+      {
+        name: 'Deep Learning',
+        courses: ['Neural Networks', 'CNN Basics', 'RNN & LSTM'],
+      },
+    ],
+  },
+  {
+    name: 'Cloud Computing',
+    subcategories: [
+      {
+        name: 'AWS',
+        courses: ['AWS Fundamentals', 'AWS Solutions Architect'],
+      },
+      {
+        name: 'Azure',
+        courses: ['Azure Basics', 'Deploying on Azure'],
+      },
+    ],
+  },
+];
+
+export default categories;
